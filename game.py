a60d9caddb577214ed55deff44b83c66e4eaa99f
alsarazin <alsarazin@users.noreply.github.com>
2014-11-19 13:53:40 +0100

    evolution function finished

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/python
 
 class Cell:
 	"""A cell"""
@@ -22,7 +22,7 @@ class Cell:
 class Board:
 	"""Class defining a  board"""
 	lines = []
-	size = 0	
+	size = 0
 
 	def __init__(self, size):
 		self.size = size
@@ -32,17 +32,47 @@ class Board:
 				aLine.append(Cell(0))
 			self.lines.append(aLine) 	
 
-	#def evolution(self,line,column):
-		#if line == 0 and column == 0: #if up-left corner
-			#TODO				
-				
-	
+	def evolution(self,line,column):
+		alives = 0
+		
+		if line == 0: #if up line
+			cell_down = self.lines[line+1][column] 
+			
+			if column == 0:	#if up-left corner
+				alives = countAlives([self.lines[line][column+1],cell_down,self.lines[line+1][column+1]])
+			
+			elif column == self.size-1: #if up-right corner
+				alives = countAlives([self.lines[line][column-1],cell_down,self.lines[line+1][column-1]])
+			
+			else:
+				alives = countAlives([self.lines[line][column-1],self.lines[line][column+1],self.lines[line+1][column-1],cell_down,self.lines[line+1][column+1]])
+		
+		elif line == self.size-1: #if bottom line
+			cell_up = self.lines[line-1][column]
+			
+			if column == 0: #if down-left corner
+				alives = countAlives([self.lines[line][column+1], cell_up, self.lines[line-1][column+1]])
+			
+			elif column == self.size-1: #if down-right corner
+				alives = countAlives([self.lines[line][column-1], cell_up, self.lines[line-1][column-1]])
+			
+			else:
+				alives = countAlives([self.lines[line][column-1], self.lines[line][column+1], self.lines[line-1][column-1], cell_up, self.lines[line-1][column+1]])
+		
+		elif column == 0: #if left column except corners
+			alives = countAlives([self.lines[line-1][column],self.lines[line-1][column+1],self.lines[line][column+1],self.lines[line+1][column+1], self.lines[line+1][column]])
+		
+		elif column == self.size-1: # if right column except corners
+			alives = countAlives([self.lines[line-1][column],self.lines[line-1][column-1],self.lines[line][column-1],self.lines[line+1][column-1], self.lines[line+1][column]])
+		
+		else: #all the other cells
+			alives = countAlives([self.lines[line-1][column-1],self.lines[line-1][column],self.lines[line-1][column+1],self.lines[line][column-1],self.lines[line][column+1],self.lines[line+1][column-1], self.lines[line+1][column],self.lines[line+1][column+1]])
+
+		#TODO change state of cell in function of alives 
+
 def countAlives(cells):
 	alives = 0
 	for cell in cells:
 		if cell.getState():
 			alives += 1
 	return alives
-
-board = Board(2)
-print(str(countAlives([Cell(0),Cell(1), Cell(0)])))
